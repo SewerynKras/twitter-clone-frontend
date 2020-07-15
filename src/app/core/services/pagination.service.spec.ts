730ac50d48242db3cfaf9086d1984f998437bfbf
Seweryn Kras <krasseweryn@gmail.com>
2020-07-15 21:45:08 +0200

    getPage implementation

--- src/app/core/services/pagination.service.spec.ts
+++ src/app/core/services/pagination.service.spec.ts
@@ -1,20 +1,51 @@
+import { ListResponse } from './../../shared/models/response.model';
+import { environment } from './../../../environments/environment';
 import { TestBed } from '@angular/core/testing';
 
 import { PaginationService } from './pagination.service';
+import {
+  HttpTestingController,
+  HttpClientTestingModule,
+} from '@angular/common/http/testing';
 
 describe('PaginationService', () => {
   let service: PaginationService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PaginationService],
+    });
     service = TestBed.inject(PaginationService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should retrieve the next page correctly', () => {});
+  it('should retrieve the page correctly', () => {
+    service
+      .getPage(`${environment.backendURL}/endpoint/`, 2)
+      .subscribe((response) => {
+        expect(response).toEqual(data);
+      });
 
-  it('should retrieve the previous page correctly', () => {});
+    let data: ListResponse<any> = {
+      count: 1,
+      next: 3,
+      previous: 1,
+      results: [],
+    };
+    const req = httpMock.expectOne(
+      `${environment.backendURL}/endpoint/?page=2`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(data);
+  });
 });
--- src/app/core/services/pagination.service.ts
+++ src/app/core/services/pagination.service.ts
@@ -1,9 +1,20 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ListResponse } from './../../shared/models/response.model';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaginationService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  /**
+   * Retrieves the selected page from a paginated response.
+   * @param baseUrl string
+   * @param pageNum number
+   */
+  getPage(baseUrl: string, pageNum: number): Observable<ListResponse<any>> {
+    return this.http.get<ListResponse<any>>(`${baseUrl}?page=${pageNum}`);
+  }
 }
