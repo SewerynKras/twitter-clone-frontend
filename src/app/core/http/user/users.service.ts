777750779a5643f4186100c84c26f42d44fd992d
Seweryn Kras <seweryn@DESKTOP-PVM6IS5.localdomain>
2020-07-15 16:23:16 +0200

    getMyProfile

--- src/app/core/http/user/users.service.ts
+++ src/app/core/http/user/users.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {
+  UserProfileResponse,
+  UserProfilePOSTBody,
+  UserProfilePATCHBody,
+} from '../../../shared/models/user.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
+  public baseUrl = `${environment.backendURL}/users/`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Retrieves information about the currently logged in users profile.
+   * This should only be called after successful login
+   */
+  getMyProfile(): Observable<UserProfileResponse> {
+    let url = `${this.baseUrl}/getMyProfile/`;
+    return this.http.get<UserProfileResponse>(url);
+  }
 }
