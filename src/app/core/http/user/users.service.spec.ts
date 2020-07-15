57097cb9028371006032fa6200b6d2473c001473
Seweryn Kras <krasseweryn@gmail.com>
2020-07-15 16:23:16 +0200

    getMyProfile

--- src/app/core/http/user/users.service.spec.ts
+++ src/app/core/http/user/users.service.spec.ts
@@ -1,20 +1,43 @@
+import { environment } from './../../../../environments/environment';
+import { UserProfileMockResponse } from './../../mocks/user.mock';
 import { TestBed } from '@angular/core/testing';
 
 import { UsersService } from './users.service';
+import {
+  HttpTestingController,
+  HttpClientTestingModule,
+} from '@angular/common/http/testing';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UsersService],
+    });
     service = TestBed.inject(UsersService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should retrieve my profile', () => {});
+  it('should retrieve my profile', () => {
+    service.getMyProfile().subscribe((profile) => {
+      expect(profile).toEqual(UserProfileMockResponse);
+    });
+
+    const req = httpMock.expectOne(`${service.baseUrl}/getMyProfile/`);
+    expect(req.request.method).toBe('GET');
+    req.flush(UserProfileMockResponse);
+  });
 
   it('should retrieve a list of profiles', () => {});
 
