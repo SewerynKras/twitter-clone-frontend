457e65cf70700e0bde6bf04d0e8854707bf8fa2b
Seweryn Kras <krasseweryn@gmail.com>
2020-07-17 19:57:27 +0200

    tests and mocks

---
+++ src/app/core/mocks/like.mock.ts
@@ -0,0 +1,9 @@
+import { LikePOSTBody, LikeResponse } from './../../shared/models/like.model';
+
+export const LikePOSTBodyMock: LikePOSTBody = {
+  tweet_id: '11111111-1111-1111-1111-111111111111',
+};
+
+export const LikeResponseMock: LikeResponse = {
+  created: true,
+};
