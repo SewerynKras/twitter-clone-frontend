c693901c497e5bd4811a9d473bea598d90ded3a0
Seweryn Kras <krasseweryn@gmail.com>
2020-07-17 20:09:09 +0200

    create and delete methods

--- src/app/core/http/like/likes.service.ts
+++ src/app/core/http/like/likes.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../../../environments/environment';
+import {
+  LikeResponse,
+  LikePOSTBody,
+} from './../../../shared/models/like.model';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LikesService {
+  baseUrl = `${environment.backendURL}/likes`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Creates a like object for the given tweet
+   * @param tweet_id string
+   */
+  createLike(tweet_id: string): Observable<LikeResponse> {
+    let body: LikePOSTBody = {
+      tweet_id: tweet_id,
+    };
+    let url = `${this.baseUrl}/`;
+    return this.http.post<LikeResponse>(url, body);
+  }
+  /**
+   * Deletes a like object for the given tweet
+   * @param tweet_id string
+   */
+  deleteLike(tweet_id: string): Observable<void> {
+    let url = `${this.baseUrl}/${tweet_id}/`;
+    return this.http.delete<void>(url);
+  }
 }
