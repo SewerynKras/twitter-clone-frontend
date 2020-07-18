dde952865eee49825f8215b61a74d79e12ee2d0e
Seweryn Kras <seweryn@DESKTOP-PVM6IS5.localdomain>
2020-07-18 22:41:36 +0200

    tweet object/list modules

---
+++ src/app/modules/tweet-object/components/tweet-object/tweet-object.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tweet-object',
+  templateUrl: './tweet-object.component.html',
+  styleUrls: ['./tweet-object.component.scss']
+})
+export class TweetObjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
