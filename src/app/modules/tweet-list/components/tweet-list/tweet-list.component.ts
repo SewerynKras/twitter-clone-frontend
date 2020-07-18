7d916817800fd197266f40240d1776fce24ffc33
Seweryn Kras <krasseweryn@gmail.com>
2020-07-18 23:46:24 +0200

    barebones tweet template

--- src/app/modules/tweet-list/components/tweet-list/tweet-list.component.ts
+++ src/app/modules/tweet-list/components/tweet-list/tweet-list.component.ts
@@ -1,15 +1,21 @@
+import {
+  TweetResponseListMockPage1,
+  TweetResponseListMockPage2,
+} from './../../../../core/mocks/tweet.mock';
+import { TweetResponse } from './../../../../shared/models/tweet.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tweet-list',
   templateUrl: './tweet-list.component.html',
-  styleUrls: ['./tweet-list.component.scss']
+  styleUrls: ['./tweet-list.component.scss'],
 })
 export class TweetListComponent implements OnInit {
+  tweets: TweetResponse[] = TweetResponseListMockPage1['results'].concat(
+    TweetResponseListMockPage2['results']
+  );
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/modules/tweet-object/components/tweet-object/tweet-object.component.ts
+++ src/app/modules/tweet-object/components/tweet-object/tweet-object.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { TweetResponse } from './../../../../shared/models/tweet.model';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tweet-object',
   templateUrl: './tweet-object.component.html',
-  styleUrls: ['./tweet-object.component.scss']
+  styleUrls: ['./tweet-object.component.scss'],
 })
 export class TweetObjectComponent implements OnInit {
+  @Input() tweet: TweetResponse;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
