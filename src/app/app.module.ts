6c52cb60024823acc290417b5457d4d08219aa30
Seweryn Kras <krasseweryn@gmail.com>
2020-07-18 17:19:44 +0200

    base layout setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,24 @@ import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { LayoutComponent } from './core/layout/layout.component';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { MenuComponent } from './core/layout/menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatGridListModule, MatCardModule],
+  declarations: [AppComponent, LayoutComponent, HeaderComponent, MenuComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    FlexLayoutModule,
+  ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
