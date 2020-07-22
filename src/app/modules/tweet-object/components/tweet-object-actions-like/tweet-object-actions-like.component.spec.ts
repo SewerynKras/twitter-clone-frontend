4f8e65c9b08d73b1568b77ddc35314f2549efd2e
Seweryn Kras <krasseweryn@gmail.com>
2020-07-22 22:05:53 +0200

    toggle like method + tests

--- src/app/modules/tweet-object/components/tweet-object-actions-like/tweet-object-actions-like.component.spec.ts
+++ src/app/modules/tweet-object/components/tweet-object-actions-like/tweet-object-actions-like.component.spec.ts
@@ -1,3 +1,4 @@
+import { TweetResponseMock } from './../../../../core/mocks/tweet.mock';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TweetObjectActionsLikeComponent } from './tweet-object-actions-like.component';
@@ -15,10 +16,25 @@ describe('TweetObjectActionsLikeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TweetObjectActionsLikeComponent);
     component = fixture.componentInstance;
+    component.tweet = TweetResponseMock;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should like the tweet', () => {
+    component.tweet.is_liked = false;
+    spyOn(component['likeService'], 'createLike');
+    component.toggleLike();
+    expect(component['likeService'].createLike).toHaveBeenCalled();
+  });
+
+  it('should un-like the tweet', () => {
+    component.tweet.is_liked = true;
+    spyOn(component['likeService'], 'deleteLike');
+    component.toggleLike();
+    expect(component['likeService'].deleteLike).toHaveBeenCalled();
   });
 });
