e18e72f816cfaba5eaec22649116b60fea784b9e
Seweryn Kras <seweryn@DESKTOP-PVM6IS5.localdomain>
2020-07-23 18:55:19 +0200

    basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
+import { TweetListRoutes } from './modules/tweet-list/tweet-list.routes';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tweets/', children: TweetListRoutes },
+  // { path: 'tweets/:id/', component: TweetSingleRoutes },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/modules/tweet-list/tweet-list.routes.ts
@@ -0,0 +1,5 @@
+import { TweetListComponent } from './components/tweet-list/tweet-list.component';
+import { Routes } from '@angular/router';
+export const TweetListRoutes: Routes = [
+  { path: '', component: TweetListComponent },
+];
