a770f9999d8e8534cb7b98c842f382268881f197
Seweryn Kras <krasseweryn@gmail.com>
2020-07-23 17:21:43 +0200

    highlight on hover classes

--- src/app/modules/tweet-object/components/tweet-object-actions-share/tweet-object-actions-share.component.ts
+++ src/app/modules/tweet-object/components/tweet-object-actions-share/tweet-object-actions-share.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TweetObjectActionsShareComponent implements OnInit {
   @Input() tweet: TweetResponse;
-
+  controlDisabled = false;
   constructor() {}
 
   ngOnInit(): void {}
