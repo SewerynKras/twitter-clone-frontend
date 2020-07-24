9c777a8f73ef3d7fd7bce26cd76db3622fb3b201
Seweryn Kras <seweryn@DESKTOP-PVM6IS5.localdomain>
2020-07-24 17:50:24 +0200

    created info

--- src/app/modules/tweet-page/components/tweet-page-created-info/tweet-page-created-info.component.spec.ts
+++ src/app/modules/tweet-page/components/tweet-page-created-info/tweet-page-created-info.component.spec.ts
@@ -1,3 +1,4 @@
+import { TweetResponseMock } from './../../../../core/mocks/tweet.mock';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TweetPageCreatedInfoComponent } from './tweet-page-created-info.component';
@@ -8,18 +9,29 @@ describe('TweetPageCreatedInfoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TweetPageCreatedInfoComponent ]
-    })
-    .compileComponents();
+      declarations: [TweetPageCreatedInfoComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TweetPageCreatedInfoComponent);
     component = fixture.componentInstance;
+    component.tweet = TweetResponseMock;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should calculate the date correctly', () => {
+    component.tweet.created_date = new Date('1995-12-17T03:24:00');
+    const result = component.getDateCreated();
+    expect(result).toEqual('17 Dec 1995');
+  });
+  it('should calculate the time correctly', () => {
+    component.tweet.created_date = new Date('1995-12-17T03:24:00');
+    const result = component.getTimeCreated();
+    expect(result).toEqual('3:24');
   });
 });
