f1b8f66214439e8a019f73ac20317cdb8366e9db
Seweryn Kras <krasseweryn@gmail.com>
2020-07-26 20:10:13 +0200

    modules init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { LoginPageRoutes } from './modules/login-page/login-page.routes';
 import { TweetPageRoutes } from './modules/tweet-page/tweet-object.routes';
 import { TweetListRoutes } from './modules/tweet-list/tweet-list.routes';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', children: LoginPageRoutes },
   { path: 'tweets', children: TweetListRoutes },
   { path: 'tweets/:tweet_id', children: TweetPageRoutes },
 ];
---
+++ src/app/modules/login-page/login-page.routes.ts
@@ -0,0 +1,5 @@
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { Routes } from '@angular/router';
+export const LoginPageRoutes: Routes = [
+  { path: '', component: LoginPageComponent },
+];
