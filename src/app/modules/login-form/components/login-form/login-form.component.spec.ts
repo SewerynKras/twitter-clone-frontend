034fa21c7c92345c2717a8c71371f3fa4b1e976b
Seweryn Kras <seweryn@DESKTOP-PVM6IS5.localdomain>
2020-07-28 10:48:33 +0200

    login form tests

--- src/app/modules/login-form/components/login-form/login-form.component.spec.ts
+++ src/app/modules/login-form/components/login-form/login-form.component.spec.ts
@@ -1,6 +1,13 @@
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TokenResponseMock } from './../../../../core/mocks/token.mock';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginFormComponent } from './login-form.component';
+import {
+  HttpTestingController,
+  HttpClientTestingModule,
+} from '@angular/common/http/testing';
 
 describe('LoginFormComponent', () => {
   let component: LoginFormComponent;
@@ -8,9 +15,9 @@ describe('LoginFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginFormComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      declarations: [LoginFormComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +28,35 @@ describe('LoginFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call authenticate when submitting the form', () => {
+    spyOn(component, 'authenticate');
+    component.onSubmit();
+    expect(component.authenticate).toHaveBeenCalled();
+  });
+
+  it('should get all the forms data', () => {
+    component.loginForm.controls['username'].setValue('someUsername');
+    component.loginForm.controls['password'].setValue('somePassword123');
+    const result = component.getFormData();
+    expect(result['username']).toEqual('someUsername');
+    expect(result['password']).toEqual('somePassword123');
+  });
+
+  it('should authenticate the user correctly', () => {
+    component.loginForm.controls['username'].setValue('someUsername');
+    component.loginForm.controls['password'].setValue('somePassword123');
+    spyOn(component['authService'], 'login').and.returnValue(
+      of(TokenResponseMock)
+    );
+    spyOn(component['usersService'], 'getMyProfile').and.returnValue(of(null));
+
+    component.authenticate();
+
+    expect(component['authService'].login).toHaveBeenCalledWith(
+      'someUsername',
+      'somePassword123'
+    );
   });
 });
