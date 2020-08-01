3b596d39d206fa1ab1cf83b2ea6fd12e6ee45701
Seweryn Kras <seweryn@DESKTOP-PVM6IS5.localdomain>
2020-08-01 20:07:57 +0200

    tweet creation missing tests

--- src/app/modules/tweet-creation/components/tweet-creation/tweet-creation.component.spec.ts
+++ src/app/modules/tweet-creation/components/tweet-creation/tweet-creation.component.spec.ts
@@ -1,4 +1,15 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import { TweetResponseMock } from 'src/app/core/mocks/tweet.mock';
+import { TweetCreationTextareaComponent } from './../tweet-creation-textarea/tweet-creation-textarea.component';
+import { TweetCreationActionsSubmitComponent } from './../tweet-creation-actions-submit/tweet-creation-actions-submit.component';
+import { TweetCreationActionsImageComponent } from './../tweet-creation-actions-image/tweet-creation-actions-image.component';
+import { TweetCreationImagePreviewComponent } from './../tweet-creation-image-preview/tweet-creation-image-preview.component';
+import { TweetCreationActionsEmojiComponent } from './../tweet-creation-actions-emoji/tweet-creation-actions-emoji.component';
+import { TweetCreationActionsComponent } from './../tweet-creation-actions/tweet-creation-actions.component';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TweetCreationComponent } from './tweet-creation.component';
@@ -6,21 +17,75 @@ import { TweetCreationComponent } from './tweet-creation.component';
 describe('TweetCreationComponent', () => {
   let component: TweetCreationComponent;
   let fixture: ComponentFixture<TweetCreationComponent>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      declarations: [TweetCreationComponent],
+      declarations: [
+        TweetCreationComponent,
+        TweetCreationActionsComponent,
+        TweetCreationActionsEmojiComponent,
+        TweetCreationImagePreviewComponent,
+        TweetCreationActionsImageComponent,
+        TweetCreationActionsSubmitComponent,
+        TweetCreationTextareaComponent,
+      ],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TweetCreationComponent);
     component = fixture.componentInstance;
+    httpMock = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get the tweets body', () => {
+    spyOn(component['tweetTextarea'], 'getTextareaValue').and.returnValue(
+      'some text 123'
+    );
+    const testFile = new File([''], 'someFile');
+    component.selectedFile = testFile;
+    const result = component.getTweetBody();
+    expect(result.text).toEqual('some text 123');
+    expect(result.image).toEqual(testFile);
+  });
+
+  it('should create the tweet', () => {
+    spyOn(component['tweetsService'], 'createTweet').and.returnValue(
+      of({ ...TweetResponseMock })
+    );
+    spyOn(component, 'clearAllControls');
+    spyOn(component.tweetCreated, 'emit');
+
+    component.createTweet();
+    expect(component['tweetsService'].createTweet).toHaveBeenCalled();
+    expect(component.tweetCreated.emit).toHaveBeenCalled();
+    expect(component.clearAllControls).toHaveBeenCalled();
+  });
+
+  it('should clear all controls', () => {
+    spyOn(component.tweetTextarea, 'clearControl');
+    spyOn(component, 'removeImage');
+    component.clearAllControls();
+    expect(component.tweetTextarea.clearControl).toHaveBeenCalled();
+    expect(component.removeImage).toHaveBeenCalled();
+  });
+
+  it('should remove the image', () => {
+    spyOn(component.imagePreview, 'clearControl');
+    component.selectedFile = new File([''], 'f');
+    component.removeImage();
+    expect(component.selectedFile).toEqual(undefined);
+    expect(component.imagePreview.clearControl).toHaveBeenCalled();
   });
 });
