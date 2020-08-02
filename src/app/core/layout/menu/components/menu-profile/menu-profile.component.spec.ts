756f53784a231045a6f43d2fb38155148da0e2ea
Seweryn Kras <krasseweryn@gmail.com>
2020-08-02 09:45:59 +0200

    init test

---
+++ src/app/core/layout/menu/components/menu-profile/menu-profile.component.spec.ts
@@ -0,0 +1,35 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MenuProfileComponent } from './menu-profile.component';
+
+describe('MenuProfileComponent', () => {
+  let component: MenuProfileComponent;
+  let fixture: ComponentFixture<MenuProfileComponent>;
+  let httpMock: HttpTestingController;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [MenuProfileComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MenuProfileComponent);
+    httpMock = TestBed.inject(HttpTestingController);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should not get the users profile if they're not logged in", () => {
+    component.user$;
+  });
+});
