bcdbbd8eb00f14deaeb93f08a0f9c99a1320d60c
Seweryn Kras <seweryn@DESKTOP-PVM6IS5.localdomain>
2020-08-02 11:37:49 +0200

    menu button selected property + tests

--- src/app/core/layout/menu/components/menu-main/menu-main.component.ts
+++ src/app/core/layout/menu/components/menu-main/menu-main.component.ts
@@ -1,4 +1,4 @@
-import { buttonSpecInterface } from './../menu-button/menu-button.component';
+import { ButtonSpecInterface } from './../menu-button/menu-button.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,38 +7,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-main.component.scss'],
 })
 export class MenuMainComponent implements OnInit {
-  buttonSpecs: buttonSpecInterface[] = [
-    { icon_name: 'home', text: 'Home' },
+  buttonSpecs: ButtonSpecInterface[] = [
+    {
+      icon_name: 'home',
+      text: 'Home',
+      selected: true,
+    },
     {
       icon_name: 'share',
       text: 'Explore',
+      selected: false,
     },
     {
       icon_name: 'notifications_none',
       text: 'Notifications',
+      selected: false,
     },
     {
       icon_name: ' mail_outline',
       text: 'Messages',
+      selected: false,
     },
     {
       icon_name: 'bookmark_border',
       text: 'Bookmarks',
+      selected: false,
     },
     {
       icon_name: 'list_alt',
       text: 'Lists',
+      selected: false,
     },
     {
       icon_name: 'person_outline',
       text: 'Profile',
+      selected: false,
     },
     {
       icon_name: 'more_horiz',
       text: 'More',
+      selected: false,
     },
   ];
   constructor() {}
 
   ngOnInit(): void {}
+
+  /**
+   * Marks the given button as selected and removes the
+   * selected flag from the previously selected button.
+   * NOTE: This method assumes that all buttons have unique `text` properties.
+   */
+  selectButton(spec: ButtonSpecInterface): void {
+    let oldSelectedButton = this.buttonSpecs.filter(
+      (obj) => obj.selected === true
+    )[0]; //only 1 match is expected
+
+    let oldSelectedButtonIndex = this.buttonSpecs
+      .map((obj) => obj.text)
+      .indexOf(oldSelectedButton.text);
+
+    let newSelectedButtonIndex = this.buttonSpecs
+      .map((obj) => obj.text)
+      .indexOf(spec.text);
+
+    // remove the old selected flag
+    this.buttonSpecs[oldSelectedButtonIndex].selected = false;
+
+    // create new selected flag
+    this.buttonSpecs[newSelectedButtonIndex].selected = true;
+  }
 }
