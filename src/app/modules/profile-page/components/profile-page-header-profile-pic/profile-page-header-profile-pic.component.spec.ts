2865e28f07badfa0c7be8478eae30bb58634af59
Seweryn Kras <krasseweryn@gmail.com>
2020-08-10 18:43:22 +0200

    missing test

--- src/app/modules/profile-page/components/profile-page-header-profile-pic/profile-page-header-profile-pic.component.spec.ts
+++ src/app/modules/profile-page/components/profile-page-header-profile-pic/profile-page-header-profile-pic.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfilePageHeaderProfilePicComponent } from './profile-page-header-profile-pic.component';
+import { UserProfileMockResponse } from 'src/app/core/mocks/user.mock';
 
 describe('ProfilePageHeaderProfilePicComponent', () => {
   let component: ProfilePageHeaderProfilePicComponent;
@@ -8,18 +9,28 @@ describe('ProfilePageHeaderProfilePicComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfilePageHeaderProfilePicComponent ]
-    })
-    .compileComponents();
+      declarations: [ProfilePageHeaderProfilePicComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProfilePageHeaderProfilePicComponent);
     component = fixture.componentInstance;
+    component.user = { ...UserProfileMockResponse };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should resize the image correctly', () => {
+    spyOn(component['resize'], 'applyTransform');
+    component.getImageResized();
+    expect(component['resize'].applyTransform).toHaveBeenCalledWith(
+      { ...UserProfileMockResponse }.image_url,
+      140,
+      140
+    );
   });
 });
