a531ca33f4da2abc1f4cc7265840e1301e29115f
Seweryn Kras <seweryn@DESKTOP-PVM6IS5.localdomain>
2020-08-11 18:29:56 +0200

    followers/ing counter in profile

--- src/app/modules/profile-page/components/profile-page-followers/profile-page-followers.component.ts
+++ src/app/modules/profile-page/components/profile-page-followers/profile-page-followers.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { UserProfileResponse } from 'src/app/shared/models/user.model';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-profile-page-followers',
   templateUrl: './profile-page-followers.component.html',
-  styleUrls: ['./profile-page-followers.component.scss']
+  styleUrls: ['./profile-page-followers.component.scss'],
 })
 export class ProfilePageFollowersComponent implements OnInit {
+  @Input() user: UserProfileResponse;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
