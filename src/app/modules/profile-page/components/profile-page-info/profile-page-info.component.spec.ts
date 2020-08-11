92554cd62613fe38b09ce1bb1a7033a0baeded0a
Seweryn Kras <seweryn@DESKTOP-PVM6IS5.localdomain>
2020-08-11 18:05:32 +0200

    user's website navigation

--- src/app/modules/profile-page/components/profile-page-info/profile-page-info.component.spec.ts
+++ src/app/modules/profile-page/components/profile-page-info/profile-page-info.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfilePageInfoComponent } from './profile-page-info.component';
+import { UserProfileMockResponse } from 'src/app/core/mocks/user.mock';
 
 describe('ProfilePageInfoComponent', () => {
   let component: ProfilePageInfoComponent;
@@ -8,18 +9,40 @@ describe('ProfilePageInfoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfilePageInfoComponent ]
-    })
-    .compileComponents();
+      declarations: [ProfilePageInfoComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProfilePageInfoComponent);
     component = fixture.componentInstance;
+    component.user = { ...UserProfileMockResponse };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to the users website', () => {
+    var _window = {
+      location: {
+        assign: jasmine.createSpy(),
+      },
+    };
+    component.user.website = 'https://test.test';
+    component.navigateToWebsite(_window);
+    expect(_window.location.assign).toHaveBeenCalledWith('https://test.test');
+  });
+
+  it('should add the "https://" prefix if its not present in the users website during navigation', () => {
+    var _window = {
+      location: {
+        assign: jasmine.createSpy(),
+      },
+    };
+    component.user.website = 'test.test';
+    component.navigateToWebsite(_window);
+    expect(_window.location.assign).toHaveBeenCalledWith('https://test.test');
   });
 });
