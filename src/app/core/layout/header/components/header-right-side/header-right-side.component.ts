423ec317162af5e9ba9b78027638c6c4468de428
Seweryn Kras <seweryn@DESKTOP-PVM6IS5.localdomain>
2020-08-12 19:14:15 +0200

    search bar in header

--- src/app/core/layout/header/components/header-right-side/header-right-side.component.ts
+++ src/app/core/layout/header/components/header-right-side/header-right-side.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header-right-side',
   templateUrl: './header-right-side.component.html',
-  styleUrls: ['./header-right-side.component.scss']
+  styleUrls: ['./header-right-side.component.scss'],
 })
 export class HeaderRightSideComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
