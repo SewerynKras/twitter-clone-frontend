7fdf67bb21782c5dd3c52b9d58eed3064fcc7278
Seweryn Kras <krasseweryn@gmail.com>
2020-08-12 11:13:30 +0200

    move tweet list page to seperate module

---
+++ src/app/modules/tweet-list-page/tweet-list-page.routes.ts
@@ -0,0 +1,5 @@
+import { TweetListPageComponent } from './components/tweet-list-page/tweet-list-page.component';
+import { Routes } from '@angular/router';
+export const TweetListPageRoutes: Routes = [
+  { path: '', component: TweetListPageComponent },
+];
