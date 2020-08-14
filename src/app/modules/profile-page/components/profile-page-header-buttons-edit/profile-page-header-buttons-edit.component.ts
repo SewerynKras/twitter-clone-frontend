b6c94fd5b6dbdd48fde8ce5762db2d2c205c8215
Seweryn Kras <seweryn@DESKTOP-PVM6IS5.localdomain>
2020-08-14 12:17:48 +0200

    button display logic

--- src/app/modules/profile-page/components/profile-page-header-buttons-edit/profile-page-header-buttons-edit.component.ts
+++ src/app/modules/profile-page/components/profile-page-header-buttons-edit/profile-page-header-buttons-edit.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile-page-header-buttons-edit',
   templateUrl: './profile-page-header-buttons-edit.component.html',
-  styleUrls: ['./profile-page-header-buttons-edit.component.scss']
+  styleUrls: ['./profile-page-header-buttons-edit.component.scss'],
 })
 export class ProfilePageHeaderButtonsEditComponent implements OnInit {
+  controlDisabled = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
