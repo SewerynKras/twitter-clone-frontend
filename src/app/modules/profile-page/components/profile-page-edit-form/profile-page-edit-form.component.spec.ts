99a45d7614651630d5861365806507d210294f11
Seweryn Kras <seweryn@DESKTOP-PVM6IS5.localdomain>
2020-08-15 19:54:03 +0200

    form components tests setup

--- src/app/modules/profile-page/components/profile-page-edit-form/profile-page-edit-form.component.spec.ts
+++ src/app/modules/profile-page/components/profile-page-edit-form/profile-page-edit-form.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfilePageEditFormComponent } from './profile-page-edit-form.component';
+import { UserProfileMockResponse } from 'src/app/core/mocks/user.mock';
 
 describe('ProfilePageEditFormComponent', () => {
   let component: ProfilePageEditFormComponent;
@@ -8,9 +9,8 @@ describe('ProfilePageEditFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfilePageEditFormComponent ]
-    })
-    .compileComponents();
+      declarations: [ProfilePageEditFormComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +21,20 @@ describe('ProfilePageEditFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get the form values', () => {
+    var prof = { ...UserProfileMockResponse };
+    component.editForm.controls['username'].setValue(prof.username);
+    component.editForm.controls['bio'].setValue(prof.bio);
+    component.editForm.controls['display_name'].setValue(prof.display_name);
+    component.editForm.controls['website'].setValue(prof.website);
+    component.editForm.controls['birth_date'].setValue(prof.birth_date);
+    var result = component.getValue();
+    expect(result.bio).toEqual(prof.bio);
+    expect(result.birth_date).toEqual(prof.birth_date);
+    expect(result.username).toEqual(prof.username);
+    expect(result.display_name).toEqual(prof.display_name);
+    expect(result.website).toEqual(prof.website);
   });
 });
