ce04db537468b19b38f5a6ca44cc6d293a3810ad
Seweryn Kras <seweryn@DESKTOP-PVM6IS5.localdomain>
2020-08-15 18:00:28 +0200

    image components implementation and tests

---
+++ src/app/modules/profile-page/components/profile-page-edit-form-image/profile-page-edit-form-image.component.spec.ts
@@ -0,0 +1,58 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfilePageEditFormImageComponent } from './profile-page-edit-form-image.component';
+
+describe('ProfilePageEditFormImageComponent', () => {
+  let component: ProfilePageEditFormImageComponent;
+  let fixture: ComponentFixture<ProfilePageEditFormImageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProfilePageEditFormImageComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProfilePageEditFormImageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should reset the control to its default state', () => {
+    component['image'] = <File>new Blob(['test']);
+    component.imgSrc = '123';
+    component.resetImg();
+    expect(component['image']).toEqual(undefined);
+    expect(component.imgSrc).toEqual('assets/profilepic.png');
+  });
+
+  it('should preview the image', () => {
+    const blob = new Blob(['test123']);
+    const file = <File>blob;
+    const fileList = {
+      0: file,
+      1: file,
+      length: 2,
+      item: (index: number) => file,
+    };
+    component.preview(fileList);
+    expect(component['image']).toEqual(file);
+  });
+
+  it('should get the value', () => {
+    const file = new File(['test123'], 'name');
+    component['image'] = file;
+    var result = component.getValue();
+    expect(result).toEqual(file);
+  });
+
+  it('should return null if there is no value', () => {
+    component['image'] = null;
+    var result = component.getValue();
+    expect(result).toEqual(null);
+  });
+});
---
+++ src/app/modules/profile-page/components/profile-page-edit-form-image/profile-page-edit-form-image.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-page-edit-form-image',
+  templateUrl: './profile-page-edit-form-image.component.html',
+  styleUrls: ['./profile-page-edit-form-image.component.scss'],
+})
+export class ProfilePageEditFormImageComponent implements OnInit {
+  constructor() {}
+
+  private image: File | null;
+  imgSrc: string | ArrayBuffer;
+
+  ngOnInit(): void {
+    this.resetImg();
+  }
+
+  /**
+   * Resets the control to it's default state.
+   */
+  resetImg(): void {
+    this.imgSrc = 'assets/profilepic.png';
+    this.image = null;
+  }
+
+  /**
+   * Saves the filepath and parses it
+   * @param files
+   */
+  preview(files: FileList): void {
+    // if there is no file the control gets cleared
+    if (files.length === 0) {
+      this.resetImg();
+      return;
+    }
+
+    // Read the file and display it as the preview's src
+    var reader = new FileReader();
+    this.image = files[0];
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.imgSrc = reader.result;
+    };
+  }
+
+  /**
+   * Retrieves the control's value (or null if no image has been selected)
+   */
+  getValue(): File | null {
+    return this.image;
+  }
+}
