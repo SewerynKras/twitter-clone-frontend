a3596b812b2a60bc57fc9d1cc85519b595932d99
Seweryn Kras <seweryn@DESKTOP-PVM6IS5.localdomain>
2020-08-16 23:20:21 +0200

    stop propagation directive tests

--- src/app/shared/directives/stop-propagation.directive.spec.ts
+++ src/app/shared/directives/stop-propagation.directive.spec.ts
@@ -5,4 +5,13 @@ describe('StopPropagationDirective', () => {
     const directive = new StopPropagationDirective();
     expect(directive).toBeTruthy();
   });
+
+  it('should stop propagation', () => {
+    const directive = new StopPropagationDirective();
+    var event = {
+      stopPropagation: jasmine.createSpy(),
+    };
+    directive.onClick(event);
+    expect(event.stopPropagation).toHaveBeenCalled();
+  });
 });
