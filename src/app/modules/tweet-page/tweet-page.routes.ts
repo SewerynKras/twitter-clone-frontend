c2ac28b06fabb1c1d4022616fc94be809508170c
Seweryn Kras <sewerynkras@gmail.com>
2020-08-29 13:55:59 +0200

    tweet page routing module

--- src/app/modules/tweet-page/tweet-page.routes.ts
+++ src/app/modules/tweet-page/tweet-page.routes.ts
@@ -1,5 +1,11 @@
 import { TweetPageComponent } from './components/tweet-page/tweet-page.component';
-import { Routes } from '@angular/router';
-export const TweetPageRoutes: Routes = [
-  { path: '', component: TweetPageComponent },
-];
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+export const routes: Routes = [{ path: '', component: TweetPageComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TweetPageRoutingModule {}
